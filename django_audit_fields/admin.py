c7dcdf64c8b9e4b383704ba9b3b4882c4b192f84
erikvw <ew2789@gmail.com>
2020-08-19 06:55:17 -0500

    add docstring comment

--- django_audit_fields/admin.py
+++ django_audit_fields/admin.py
@@ -14,6 +14,7 @@ audit_fieldset_tuple = ("Audit", {"classes": ("collapse",), "fields": audit_fiel
 
 class ModelAdminAuditFieldsMixin:
     def save_model(self, request, obj, form, change):
+        """Update audit fields from request object before save."""
         if not change:
             obj.user_created = request.user.username
             obj.created = get_utcnow()
@@ -23,7 +24,8 @@ class ModelAdminAuditFieldsMixin:
         super().save_model(request, obj, form, change)
 
     def get_list_filter(self, request):
-        super().get_list_filter(request)
+        """Add audit fields to end of list display."""
+        list_filter = super().get_list_filter(request)
         fields = [
             "created",
             "modified",
@@ -32,12 +34,11 @@ class ModelAdminAuditFieldsMixin:
             "hostname_created",
             "hostname_modified",
         ]
-        self.list_filter = list(self.list_filter) + [
-            f for f in fields if f not in self.list_filter
-        ]
-        return self.list_filter
+        list_filter = [f for f in list_filter if f not in fields] + fields
+        return list_filter
 
     def get_readonly_fields(self, request, obj=None):
+        """Add audit fields to readonly_fields."""
         readonly_fields = super().get_readonly_fields(request, obj=obj)
         return list(readonly_fields) + [
             f for f in audit_fields if f not in readonly_fields
