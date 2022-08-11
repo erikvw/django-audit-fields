1d1a1c8b2280b6e196d680e81b59fea1c9dfac44
erikvw <ew2789@gmail.com>
2022-08-11 17:42:50 -0500

    add audit fields to list display by default

--- django_audit_fields/admin.py
+++ django_audit_fields/admin.py
@@ -13,6 +13,9 @@ audit_fieldset_tuple: Tuple[str, Dict[str, Tuple[str, ...]]] = (
 
 
 class ModelAdminAuditFieldsMixin:
+
+    include_audit_fields_in_list_display: bool = True
+
     def save_model(self, request, obj, form, change) -> None:
         """Update audit fields from request object before save."""
         if not change:
@@ -23,12 +26,26 @@ class ModelAdminAuditFieldsMixin:
             obj.modified = get_utcnow()
         super().save_model(request, obj, form, change)
 
-    def get_list_filter(self, request) -> tuple:
-        """Add audit fields to end of list display."""
+    def get_list_display(self, request) -> Tuple[str, ...]:
+        list_display = super().get_list_display(request)
+        custom_fields = (
+            (
+                "created",
+                "modified",
+                "user_created",
+                "user_modified",
+            )
+            if self.include_audit_fields_in_list_display
+            else ()
+        )
+        return tuple(f for f in list_display if f not in custom_fields) + custom_fields
+
+    def get_list_filter(self, request) -> Tuple[str, ...]:
+        """Add audit fields to list display."""
         list_filter = super().get_list_filter(request)
         return tuple(f for f in list_filter if f not in audit_fields) + audit_fields
 
-    def get_readonly_fields(self, request, obj=None) -> tuple:
+    def get_readonly_fields(self, request, obj=None) -> Tuple[str, ...]:
         """Add audit fields to readonly_fields."""
         readonly_fields = super().get_readonly_fields(request, obj=obj)
         return tuple(f for f in readonly_fields if f not in audit_fields) + audit_fields
