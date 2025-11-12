12ac392d74c2f6213eddadd67198c1d5f1acee3d
erikvw <ew2789@gmail.com>
2025-11-11 20:57:47 -0600

    update tox, gh-actions and pyproject.toml

--- runtests.py
+++ runtests.py
@@ -1,16 +1,8 @@
 #!/usr/bin/env python
-import os
-import sys
-
-import django
-from django.test.runner import DiscoverRunner
+from edc_test_settings.func_main import func_main2
 
 if __name__ == "__main__":
-    os.environ["DJANGO_SETTINGS_MODULE"] = "tests.test_settings"
-    django.setup()
-    tags = [t.split("=")[1] for t in sys.argv if t.startswith("--tag")]
-    failfast = any([True for t in sys.argv if t.startswith("--failfast")])
-    keepdb = any([True for t in sys.argv if t.startswith("--keepdb")])
-    opts = dict(failfast=failfast, tags=tags, keepdb=keepdb)
-    failures = DiscoverRunner(**opts).run_tests(["django_audit_fields.tests"])
-    sys.exit(failures)
+    func_main2(
+        "tests.test_settings",
+        "tests",
+    )
